d5b862a43b8754c0ded6dfd3e9c265d0381354d1
Yonghye Kwon <developer.0hye@gmail.com>
2023-09-30 19:23:17 +0900

    write required pkgs in install_requires

--- setup.py
+++ setup.py
@@ -9,9 +9,6 @@ from setuptools import find_packages, setup
 setup(
     name='onepose',
     version='1.0',
-    install_requires=[],
-    packages=find_packages(exclude='notebooks'),
-    extras_require={
-        'all': ['opencv-python', 'torch', 'torchvision', 'tqdm', 'numpy'],
-    },
+    install_requires=['opencv-python', 'torch', 'torchvision', 'tqdm', 'numpy'],
+    packages=find_packages(exclude='notebooks')
 )
